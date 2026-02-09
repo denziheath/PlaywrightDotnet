bb78020d35b05255a8c325fc3706cf60726ed52d
denziheath <denziheath@yahoo.com>
2026-02-09 13:08:37 -0700

    first commit

---
+++ Playwrightdemo/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Playwrightdemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
