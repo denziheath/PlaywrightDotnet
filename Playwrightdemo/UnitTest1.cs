719001bdfe18f658011ae5c0aeda05d4ec4960d2
denziheath <denziheath@yahoo.com>
2026-02-09 14:09:21 -0700

    dependencies browsers installed build created

--- Playwrightdemo/UnitTest1.cs
+++ Playwrightdemo/UnitTest1.cs
@@ -1,4 +1,7 @@
-﻿namespace Playwrightdemo;
+﻿using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+namespace Playwrightdemo;
 
 public class Tests
 {
@@ -8,8 +11,19 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
+
+        //Playwright
+        // 'await' keyword requires async method -> use 'async' in method header
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync(url: "http://www.somewebsite.com");
+        await page.ClickAsync(selector:"text=Login");
+
         Assert.Pass();
     }
 }
